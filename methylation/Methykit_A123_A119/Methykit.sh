d64e3faaf7394f4c394900a254921233ee3793ce
JinfengChen <jinfeng7chen@gmail.com>
2016-07-14 19:48:41 -0700

    filter and normalize

--- methylation/Methykit_A123_A119/Methykit.sh
+++ methylation/Methykit_A123_A119/Methykit.sh
@@ -13,6 +13,7 @@ start=`date +%s`
 
 module load bedtools
 python Methykit_pipe.py --meta methykit.A123_A119.meta --cpu $PBS_NP
+#cat A123_A119.Methykit.methykit.R | R --slave
 
 end=`date +%s`
 runtime=$((end-start))
--- methylation/Methykit_A123_A119/Methykit_pipe.py
+++ methylation/Methykit_A123_A119/Methykit_pipe.py
@@ -39,12 +39,14 @@ def cal_filesize_gzip(filename):
 
 #chunk file by line into "numbers" of files
 def chunk_files_gzip(infile, numbers):
-    filesize  = cal_filesize_gzip(infile)
-    splitline = filesize//numbers
+    #filesize  = cal_filesize_gzip(infile)
+    #splitline = filesize//numbers
     unzip_infile = os.path.splitext(infile)[0]
     if not os.path.exists(unzip_infile):
         os.system('gunzip -c %s > %s' %(infile, unzip_infile))
-    if not os.path.exists('%s_part00' %(unzip_infile)): 
+    if not os.path.exists('%s_part00' %(unzip_infile)):
+        filesize  = cal_filesize_gzip(infile)
+        splitline = filesize//numbers 
         os.system('split -l %s %s %s_part -d' %(splitline, unzip_infile, unzip_infile)) 
 
 
@@ -59,12 +61,15 @@ def read_meta(infile):
             line = line.rstrip()
             if len(line) > 2: 
                 unit = re.split(r'\t',line)
-                data[unit[0]] = unit[1]
+                name = re.split(r'\.', unit[0])[0]
+                data[unit[0]] = [name, unit[1]]
     return data
 
 #Chr1    C       1001    CHH     CT      1.0     1       1
 def convert_bsseek2_file(infile):
-    ofile = open('%s.methykit.table' %(infile), 'w') 
+    ofile1 = open('%s.methykit.CG.table' %(infile), 'w')
+    ofile2 = open('%s.methykit.CHG.table' %(infile), 'w')
+    ofile3 = open('%s.methykit.CHH.table' %(infile), 'w') 
     with open (infile, 'r') as filehd:
         for line in filehd:
             line = line.rstrip()
@@ -77,8 +82,15 @@ def convert_bsseek2_file(infile):
                 coverage= unit[7]
                 freqC   = 100 * float(unit[5])
                 freqT   = 100 - freqC
-                print >> ofile, '\t'.join(map(str,[chrbase, chrs, base, strand, coverage, freqC, freqT]))
-    ofile.close()
+                if unit[3] == 'CG':
+                    print >> ofile1, '\t'.join(map(str,[chrbase, chrs, base, strand, coverage, freqC, freqT]))
+                elif unit[3] == 'CHG':
+                    print >> ofile2, '\t'.join(map(str,[chrbase, chrs, base, strand, coverage, freqC, freqT]))
+                elif unit[3] == 'CHH':
+                    print >> ofile3, '\t'.join(map(str,[chrbase, chrs, base, strand, coverage, freqC, freqT]))
+    ofile1.close()
+    ofile2.close()
+    ofile3.close()
 
 
 def convert_bsseek2_helper(args):
@@ -95,10 +107,86 @@ def multiprocess_pool(function_helper, parameters, cpu):
         collect_list.append(x)
     return collect_list
 
+def run_methykit(meta, cpu, context, prefix):
+    files  = []
+    sample = []
+    design = []
+    for f in sorted(meta.keys()):
+        files.append('%s.methykit.%s.table' %(os.path.splitext(f)[0], context))
+        sample.append(meta[f][0])
+        design.append(meta[f][1])
+
+    Rcmd='''
+library(methylKit)
+pdf("%s.%s.pdf")
+file.list=list("%s",
+               "%s",
+               "%s",
+               "%s")
+
+# read the files to a methylRawList object: myobj
+myobj=read(file.list, sample.id=list("%s","%s","%s","%s"), assembly="MSU7",treatment=c(%s))
+
+#Get descriptive stats on methylatio
+# plot methylation statistics on samples in myobj which is a class of methylRawList
+getMethylationStats(myobj[[1]],plot=F,both.strands=F)
+getMethylationStats(myobj[[2]],plot=F,both.strands=F)
+getMethylationStats(myobj[[3]],plot=F,both.strands=F)
+getMethylationStats(myobj[[4]],plot=F,both.strands=F)
+getMethylationStats(myobj[[1]],plot=T,both.strands=F)
+getMethylationStats(myobj[[2]],plot=T,both.strands=F)
+getMethylationStats(myobj[[3]],plot=T,both.strands=F)
+getMethylationStats(myobj[[4]],plot=T,both.strands=F)
+
+getCoverageStats(myobj[[1]], plot = T, both.strands = F)
+getCoverageStats(myobj[[2]], plot = T, both.strands = F)
+getCoverageStats(myobj[[3]], plot = T, both.strands = F)
+getCoverageStats(myobj[[4]], plot = T, both.strands = F)
+
+#filter and normalize
+#filter base with fewer than 4X coverage and more than 99.9 percetile of coverage in each sample?
+filtered.myobj = filterByCoverage(myobj, lo.count = 4, lo.perc = NULL, hi.count = NULL, hi.perc = 99.9)
+normalized.myobj = normalizeCoverage(filtered.myobj)
+myobj = normalized.myobj
+
+#cluster of sample
+meth=unite(myobj)
+#a correlation matrix
+getCorrelation(meth, plot = T)
+# cluster all samples using correlation distance and plot hiarachical clustering
+clusterSamples(meth, dist="correlation", method="ward", plot=TRUE)
+# principal component anlaysis of all samples.
+PCASamples(meth)
+
+#DMC
+# calculate differential methylation p-values and q-values
+myDiff=calculateDiffMeth(meth)
+# get differentially methylated regions with 25 percent difference and qvalue<0.01
+myDiff25p=get.methylDiff(myDiff,difference=25,qvalue=0.01)
+head(myDiff25p)
+write.table(myDiff, file="%s.%s.DMC.txt", sep="\\t", quote=FALSE)
+
+#DMR
+tiles = tileMethylCounts(myobj, win.size = 200, step.size = 50, cov.bases = 5)
+meth_region = unite(tiles)
+myDiff_region =calculateDiffMeth(meth_region)
+myDiff_region25p=get.methylDiff(myDiff_region,difference=25, qvalue=0.01)
+head(myDiff_region25p)
+write.table(myDiff_region, file="%s.%s.DMR.txt", sep="\\t", quote=FALSE)
+
+dev.off()
+''' %(prefix, context, files[0], files[1], files[2], files[3], sample[0], sample[1], sample[2], sample[3], ','.join(design), prefix, context, prefix, context)
+    Rscript = '%s.%s.methykit.R' %(prefix, context)
+    ofile = open(Rscript, 'w')
+    print >> ofile, Rcmd
+    ofile.close()
+    os.system('cat %s | R --slave' %(Rscript))
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--meta')
     parser.add_argument('-c', '--cpu')
+    parser.add_argument('-p', '--project')
     parser.add_argument('-v', dest='verbose', action='store_true')
     args = parser.parse_args()
     try:
@@ -110,6 +198,8 @@ def main():
 
     if not args.cpu:
         args.cpu = 8
+    if not args.project:
+        args.project = 'A123_A119.Methykit'
 
 
     #read meta
@@ -117,7 +207,7 @@ def main():
 
     #Convert table
     for filename in sorted(meta.keys()):
-        if os.path.exists('%s.methykit.table' %(os.path.splitext(filename)[0])):
+        if os.path.exists('%s.methykit.CG.table' %(os.path.splitext(filename)[0])):
             #already have methykit table, skip all step
             continue
         chunk_files_gzip(filename, 100)
@@ -125,14 +215,26 @@ def main():
         parameters = []
         for chunk in chunks:
             parameters.append([chunk])
-        if not os.path.exists('%s_part00.methykit.table' %(os.path.splitext(filename)[0])):
+        if not os.path.exists('%s_part00.methykit.CG.table' %(os.path.splitext(filename)[0])):
             collect = multiprocess_pool(convert_bsseek2_helper, parameters, args.cpu)
-        if not os.path.exists('%s.methykit.table' %(os.path.splitext(filename)[0])):
-            merge_all = 'cat %s_part*.methykit.table > %s.methykit.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
-            os.system(merge_all)
+        if not os.path.exists('%s.methykit.CG.table' %(os.path.splitext(filename)[0])):
+            merge_CG = 'cat %s_part*.methykit.CG.table > %s.methykit.CG.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
+            merge_CHG = 'cat %s_part*.methykit.CHG.table > %s.methykit.CHG.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
+            merge_CHH = 'cat %s_part*.methykit.CHH.table > %s.methykit.CHH.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
+            os.system(merge_CG)
+            os.system(merge_CHG)
+            os.system(merge_CHH)
+        #if not os.path.exists('%s.methykit.CG.table' %(os.path.splitext(filename)[0])):
+        #    merge_CG  = 'grep "CG" %s.methykit.table > %s.methykit.CG.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
+        #    merge_CHG  = 'grep "CHG" %s.methykit.table > %s.methykit.CHG.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
+        #    merge_CHH  = 'grep "CHH" %s.methykit.table > %s.methykit.CHH.table' %(os.path.splitext(filename)[0], os.path.splitext(filename)[0])
+        #    os.system(merge_CG)
+        #    os.system(merge_CHG)
+        #    os.system(merge_CHH)
 
     #methykit
-    
+    run_methykit(meta, args.cpu, 'CG', args.project)   
+ 
 if __name__ == '__main__':
     main()
 
